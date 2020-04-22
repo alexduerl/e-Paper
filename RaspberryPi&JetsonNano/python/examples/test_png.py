4c5031491aac78c5d7feb502b78774332f67346c
alexduerl <alex.duerl@googlemail.com>
2020-04-22 21:49:04 +0200

    added energy.png

--- RaspberryPi&JetsonNano/python/examples/test_png.py
+++ RaspberryPi&JetsonNano/python/examples/test_png.py
@@ -29,7 +29,7 @@ try:
     
     logging.info("4.read bmp file on window")
     Himage2 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, 'test.png'))
+    bmp = Image.open(os.path.join(picdir, 'energy.png'))
     Himage2.paste(bmp, (50,10))
     epd.display(epd.getbuffer(Himage2))
     time.sleep(2)
