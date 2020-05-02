dfa5f915894a62e998593d77ae0305c5a1fe62c4
alexduerl <alex.duerl@googlemail.com>
2020-05-02 19:19:59 +0200

    Update temp.py
    tabs

--- RaspberryPi&JetsonNano/python/examples/temp.py
+++ RaspberryPi&JetsonNano/python/examples/temp.py
@@ -53,7 +53,7 @@ try:
     	# Datum ermitteln
     	now = datetime.datetime.now()  
 
-    	epd.Clear()     
+    	epd.Clear()
         draw = ImageDraw.Draw(Himage)
     	draw.text((5, 0), now.strftime('%d.%m.%Y') + ' ' + now.strftime('%H:%M:%S'), font = font18, fill = 0)
     	draw.line((0, 20, 400, 20), fill = 0)
