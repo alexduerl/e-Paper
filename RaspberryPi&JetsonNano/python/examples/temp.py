1c16cadae165856eea9c0f47ba2445a92fef5898
alexduerl <alex.duerl@googlemail.com>
2020-05-02 13:55:37 +0200

    Update temp.py
    while

--- RaspberryPi&JetsonNano/python/examples/temp.py
+++ RaspberryPi&JetsonNano/python/examples/temp.py
@@ -55,11 +55,9 @@ try:
     	epd.init()
     	draw.text((5, 0), now.strftime('%d.%m.%Y'), font = font18, fill = 0)
     	draw.line((0, 20, 400, 20), fill = 0)
-    	draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0)    
-    
+    	draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0)      
     	epd.display(epd.getbuffer(Himage))
     	time.sleep(2)
-    
     	logging.info("Goto Sleep...")
     	epd.sleep()
     
