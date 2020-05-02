cd659be1fcc914b040cce128a09d1c364506d19f
alexduerl <alex.duerl@googlemail.com>
2020-05-02 19:27:20 +0200

    Update temp.py
    sleep 60

--- RaspberryPi&JetsonNano/python/examples/temp.py
+++ RaspberryPi&JetsonNano/python/examples/temp.py
@@ -47,11 +47,11 @@ try:
     	# Datum ermitteln
     	now = datetime.datetime.now()  
 
-    	draw.text((5, 0), now.strftime('%d.%m.%Y') + ' ' + now.strftime('%H:%M:%S'), font = font18, fill = 0)
+    	draw.text((5, 0), now.strftime('%d.%m.%Y') + ' / ' + now.strftime('%H:%M'), font = font18, fill = 0)
     	draw.line((0, 20, 400, 20), fill = 0)
     	draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0) 
     	epd.display(epd.getbuffer(Himage))
-    	time.sleep(10)
+    	time.sleep(60)
     	#logging.info("Goto Sleep...")
     	#epd.sleep()
     
