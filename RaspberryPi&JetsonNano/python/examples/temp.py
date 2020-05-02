34754f58c3c53154fa5b04c31e17deaaa30fd3bf
alexduerl <alex.duerl@googlemail.com>
2020-05-02 19:12:48 +0200

    Update temp.py
    epd.clear

--- RaspberryPi&JetsonNano/python/examples/temp.py
+++ RaspberryPi&JetsonNano/python/examples/temp.py
@@ -42,7 +42,6 @@ try:
     logging.info("init ...")
     epd.init()
 
-
     while True:    
     	r = requests.get('http://homematic-raspi/addons/red/hello-json')
     	data = r.json()
@@ -53,11 +52,11 @@ try:
     	
     	# Datum ermitteln
     	now = datetime.datetime.now()  
-    
+
+    	epd.Clear()         
     	draw.text((5, 0), now.strftime('%d.%m.%Y'), font = font18, fill = 0)
     	draw.line((0, 20, 400, 20), fill = 0)
     	draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0) 
-    	epd.Clear()     
     	epd.display(epd.getbuffer(Himage))
     	time.sleep(60)
     	#logging.info("Goto Sleep...")
