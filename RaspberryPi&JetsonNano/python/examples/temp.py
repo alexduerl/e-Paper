ff44cf0a2a7e4123074e7c3327e32e3690f01f67
alexduerl <alex.duerl@googlemail.com>
2020-05-02 15:55:25 +0200

    Update temp.py
    epd.clear

--- RaspberryPi&JetsonNano/python/examples/temp.py
+++ RaspberryPi&JetsonNano/python/examples/temp.py
@@ -56,7 +56,8 @@ try:
     
     	draw.text((5, 0), now.strftime('%d.%m.%Y'), font = font18, fill = 0)
     	draw.line((0, 20, 400, 20), fill = 0)
-    	draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0)      
+    	draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0) 
+    	epd.Clear()     
     	epd.display(epd.getbuffer(Himage))
     	time.sleep(60)
     	#logging.info("Goto Sleep...")
