de9c267ab78d075fe23efa8f8c34a4a52e213725
alexduerl <alex.duerl@googlemail.com>
2020-05-02 15:23:25 +0200

    Update temp.py
    while

--- RaspberryPi&JetsonNano/python/examples/temp.py
+++ RaspberryPi&JetsonNano/python/examples/temp.py
@@ -48,16 +48,18 @@ try:
     #bmp = Image.open(os.path.join(picdir, 'temp.png'))
     #Himage.paste(bmp, (5,22))
 
+    logging.info("init ...")
+    epd.init()
+
+
     while True:    
-    	logging.info("init ...")
-    	epd.init()
     	draw.text((5, 0), now.strftime('%d.%m.%Y'), font = font18, fill = 0)
     	draw.line((0, 20, 400, 20), fill = 0)
     	draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0)      
     	epd.display(epd.getbuffer(Himage))
     	time.sleep(2)
-    	logging.info("Goto Sleep...")
-    	epd.sleep()
+    	#logging.info("Goto Sleep...")
+    	#epd.sleep()
     
 except IOError as e:
     logging.info(e)
