87ba3c91bae5cd7213f371ea89622419577f26b5
alexduerl <alex.duerl@googlemail.com>
2020-05-02 11:38:18 +0200

    temp.py
    while

--- RaspberryPi&JetsonNano/python/examples/temp.py
+++ RaspberryPi&JetsonNano/python/examples/temp.py
@@ -22,8 +22,8 @@ try:
     
     epd = epd4in2.EPD()
     logging.info("init and Clear")
-    epd.init()
-    epd.Clear()
+ #   epd.init()
+ #   epd.Clear()
     
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
@@ -50,17 +50,18 @@ try:
     draw = ImageDraw.Draw(Himage)
     #bmp = Image.open(os.path.join(picdir, 'temp.png'))
     #Himage.paste(bmp, (5,22))
+
+    while True:    
+    	epd.init()
+    	draw.text((5, 0), now.strftime('%d.%m.%Y'), font = font18, fill = 0)
+    	draw.line((0, 20, 400, 20), fill = 0)
+    	draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0)    
     
+    	epd.display(epd.getbuffer(Himage))
+    	time.sleep(2)
     
-    draw.text((5, 0), now.strftime('%d.%m.%Y'), font = font18, fill = 0)
-    draw.line((0, 20, 400, 20), fill = 0)
-    draw.text((25, 22), 'Temperatur: ' +  str(temp) + '°C / ' + str(hum) + '%' , font = font24, fill = 0)    
-    
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
-    
-    logging.info("Goto Sleep...")
-    epd.sleep()
+    	logging.info("Goto Sleep...")
+    	epd.sleep()
     
 except IOError as e:
     logging.info(e)
