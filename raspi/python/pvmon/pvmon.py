e1294fa4b33c94e64b14102f75dce9179fed3cca
Alex Dürl <alex.duerl@googlemail.com>
2020-05-12 10:45:32 +0200

    meters

--- raspi/python/pvmon/pvmon.py
+++ raspi/python/pvmon/pvmon.py
@@ -69,8 +69,8 @@ try:
         draw.text((5,30), 'Produktion: 59.02 kWh', font = font24, fill = 0)
         draw.text((5,55), '20%', font = font18, fill = 0)
         draw.text((305,55), '80%', font = font18, fill = 0)
-        draw.rectangle((98, 60, 301, 68), outline = 0)
-        draw.rectangle((100, 63, 120, 65), fill = 0)
+        draw.rectangle((48, 60, 351, 68), outline = 0)
+        draw.rectangle((50, 63, 80, 65), fill = 0)
         draw.text((5, 82), 'Eigenverbrauch:', font = font18, fill = 0)
 
         # Verbrauch
